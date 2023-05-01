5176f6a07ef64fed0545e5b53195bd55db37e4ff
서희연 <tjgmldus100@naver.com>
2023-05-01 10:59:29 +0900

    Feat : 패키지 dump 생성, tutor 프로토타입구현

---
+++ backend/edusetpo/src/main/java/com/seosam/edusetpo/tutor/controller/TutorController.java
@@ -0,0 +1,41 @@
+package com.seosam.edusetpo.tutor.controller;
+
+import com.seosam.edusetpo.tutor.entity.Tutor;
+import com.seosam.edusetpo.tutor.repository.TutorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController // JSON 형태 결괏값을 반환해줌(@ResponseBody 가 필요없음)
+@RequiredArgsConstructor // final 객체를 Constructor Injection 해줌(Autowired 역할)
+@RequestMapping("/tutor")
+public class TutorController {
+
+    private final TutorRepository tutorRepository;
+
+    @GetMapping("tutorList")
+    public List<Tutor> findAllTutor() {
+        return tutorRepository.findAll();
+    }
+
+    @PostMapping("tutor")
+    public Tutor signUp() {
+        final Tutor tutor = Tutor.builder()
+                .email("sodjf1@gngn.12")
+                .password("1111")
+                .name("SHY")
+                .nickname("jack")
+                .profileUrl("path")
+                .isWithdraw(false)
+                .themeIndex((short) 1)
+                .createdAt(LocalDate.now())
+                .build();
+        return tutorRepository.save(tutor);
+
+    }
+}
---
+++ backend/edusetpo/src/main/java/com/seosam/edusetpo/tutor/repository/TutorRepository.java
@@ -0,0 +1,21 @@
+package com.seosam.edusetpo.tutor.repository;
+
+import com.seosam.edusetpo.tutor.entity.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TutorRepository extends JpaRepository<Tutor, Long> {
+
+    Optional<Tutor> findByName(String name);
+
+    Optional<Tutor> findByNickname(String nickname);
+
+    Optional<Tutor> findByEmail(String email);
+
+    // 닉네임으로 유저 검색하는거(나 제외하기위해 나는 따로 찾아주는것)
+    List<Tutor> findByNicknameContainingAndNicknameNotLike(String nickname, String myNickname);
+}
