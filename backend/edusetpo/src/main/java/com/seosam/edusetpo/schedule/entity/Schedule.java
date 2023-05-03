0a279c05e693ca77e237dd5945f7d2b1c23b5bbe
OYJ <yeonjinnieoh@gmail.com>
2023-05-03 13:58:37 +0900

    Feat : schedule, lesson 엔티티 작성

--- backend/edusetpo/src/main/java/com/seosam/edusetpo/schedule/entity/Schedule.java
+++ backend/edusetpo/src/main/java/com/seosam/edusetpo/schedule/entity/Schedule.java
@@ -1,6 +1,7 @@
 package com.seosam.edusetpo.schedule.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.seosam.edusetpo.lesson.entity.Lesson;
 import lombok.*;
 
 import javax.persistence.*;
@@ -13,16 +14,17 @@ import java.time.LocalTime;
 @ToString
 @Table(name = "schedule")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "schedule_id", nullable = false)
-    private Long id;
+    private Long scheduleId;
 
     @ToString.Exclude
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="lesson_id", nullable = false)
+    @JoinColumn(name="lesson_id", nullable = false, insertable = false, updatable = false)
     private Lesson lesson;
 
     @Column(name = "lesson_day", nullable = false)
@@ -40,13 +42,12 @@ public class Schedule {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Builder
-    public Schedule(Lesson lesson, Integer lessonDay, LocalTime startTime, LocalTime endTime, Integer duration, LocalDateTime createdAt) {
-        this.lesson = lesson;
-        this.lessonDay = lessonDay;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.duration = duration;
-        this.createdAt = createdAt;
-    }
+//    public Schedule(Lesson lesson, Integer lessonDay, LocalTime startTime, LocalTime endTime, Integer duration, LocalDateTime createdAt) {
+//        this.lesson = lesson;
+//        this.lessonDay = lessonDay;
+//        this.startTime = startTime;
+//        this.endTime = endTime;
+//        this.duration = duration;
+//        this.createdAt = createdAt;
+//    }
 }
