20d98e815ea3e565f76bc69e9a2a3e9e6c18f907
yujinseo <00yujin00@naver.com>
2023-05-08 02:34:38 +0900

    Feat: 버튼 생성

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,10 +1,18 @@
 import React, { ButtonHTMLAttributes } from "react";
 import styles from "./Button.module.scss";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "red" | "success";
-}
+// import 예시 : 원하는 버튼 아래처럼 import
+// import {LongButton,ShortButtonHug,ShortButtonFixed} from "../../components/button/Button";
 
+// 사용 예시 : variant로 버튼 색 지정
+// <LongButton variant="primary"><NavLink to="tutor">강사</NavLink></LongButton>
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "danger" | "success";
+}
+// "primary" : 파랑,  "danger": 빨강,  "success" : 초록
+
+// LongButton : 25% 길이, 글자가 길면 잘리고 ...으로 변환
 export const LongButton: React.FC<ButtonProps> = ({
   variant = "primary",
   children,
@@ -22,6 +30,7 @@ export const LongButton: React.FC<ButtonProps> = ({
   );
 };
 
+//ShortButtonHug 글자 길이에 따라 늘어나는 버튼
 export const ShortButtonHug: React.FC<ButtonProps> = ({
   variant = "primary",
   children,
@@ -29,6 +38,24 @@ export const ShortButtonHug: React.FC<ButtonProps> = ({
   ...props
 }) => {
   const buttonClass = `${styles.ShortButtonHug} ${styles[variant]} ${
+    className ? className : ""
+  }`.trim();
+
+  return (
+    <button className={buttonClass} {...props}>
+      {children}
+    </button>
+  );
+};
+
+// 글자길이와 상관없이 10%의 길이, 최소 5rem,최대8rem, 글자가 길면 잘리고 ...으로 변환
+export const ShortButtonFixed: React.FC<ButtonProps> = ({
+  variant = "primary",
+  children,
+  className,
+  ...props
+}) => {
+  const buttonClass = `${styles.ShortButtonFixed} ${styles[variant]} ${
     className ? className : ""
   }`.trim();
 
--- frontend/src/pages/signUp/SignUp.tsx
+++ frontend/src/pages/signUp/SignUp.tsx
@@ -1,26 +1,16 @@
 import { NavLink } from "react-router-dom";
-import { LongButton, ShortButtonHug } from "../../components/button/Button";
 
 export const SignUp = () => {
   return (
     <div className="SignUp">
-      SignUp강사 부모
+      SignUp
       <div>
-        <LongButton variant="primary">
+        <button>
           <NavLink to="tutor">강사</NavLink>
-        </LongButton>
-        <LongButton variant="success">
+        </button>
+        <button>
           <NavLink to="parents">부모</NavLink>
-        </LongButton>
-      </div>
-      SignUp강사 부모
-      <div>
-        <ShortButtonHug variant="primary">
-          <NavLink to="tutor">dfsffdsff</NavLink>
-        </ShortButtonHug>
-        <ShortButtonHug variant="success">
-          <NavLink to="parents">부모</NavLink>
-        </ShortButtonHug>
+        </button>
       </div>
     </div>
   );
