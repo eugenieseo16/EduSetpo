f8f76c1588a526637de3976cf121596945b666c1
서희연 <tjgmldus100@naver.com>
2023-05-08 11:33:16 +0900

    Feat : 학생 상세page 설계 1%

--- frontend/src/pages/student/Student.tsx
+++ frontend/src/pages/student/Student.tsx
@@ -1,3 +1,16 @@
+import { LongButton, ShortButtonFixed } from "./../../components/button/Button";
+
 export const Student = () => {
-  return <div>Student</div>;
+  return (
+    <div>
+      <div>강잼민</div>
+      <div>
+        <ShortButtonFixed variant="danger">비활성화</ShortButtonFixed>
+        <ShortButtonFixed variant="primary">수정</ShortButtonFixed>
+      </div>
+      <div>수강과목</div>
+      
+      <LongButton variant="success">인증코드 SMS 보내기</LongButton>
+    </div>
+  );
 };
