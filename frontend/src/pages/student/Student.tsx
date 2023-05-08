f203e34a2e8e244e4793bb5037e9a07effef3fa0
서희연 <tjgmldus100@naver.com>
2023-05-08 13:13:10 +0900

    Feat : student 상세 15% 구현

--- frontend/src/pages/student/Student.tsx
+++ frontend/src/pages/student/Student.tsx
@@ -1,3 +1,4 @@
+import { NavBar } from "../../components/navBar/NavBar";
 import { LongButton, ShortButtonFixed } from "./../../components/button/Button";
 
 export const Student = () => {
@@ -9,7 +10,12 @@ export const Student = () => {
         <ShortButtonFixed variant="primary">수정</ShortButtonFixed>
       </div>
       <div>수강과목</div>
-      
+      <div>대충 수강과목들이 생겨날 곳</div>
+      <div>학생</div>
+      <div>학생번호</div>
+      <div>학부모</div>
+      <div>학부모번호</div>
+      <div>상담내역</div>
       <LongButton variant="success">인증코드 SMS 보내기</LongButton>
     </div>
   );
