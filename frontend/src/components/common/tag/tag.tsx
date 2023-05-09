f1036f07bc5a84da57591c6d298c1e96c35a37af
서희연 <tjgmldus100@naver.com>
2023-05-09 11:33:14 +0900

    Feat: 도와줘요유진맨

--- frontend/src/components/common/tag/tag.tsx
+++ frontend/src/components/common/tag/tag.tsx
@@ -1,42 +1,31 @@
-const colors = [
-  "#6FCF97",
-  "#56CCF2",
-  "#2D9CDB",
-  "#EB5757",
-  "#F2C94C",
-  "#BB6BD9",
-  "#FF9F43",
-  "#4D4D4D",
-  "#BDBDBD",
-  "#6E7B8B",
-];
+import style from "./Tag.module.scss";
 
 type Props = {
   name: string;
-  color: string;
+  idx: number;
 };
 
-import { useState } from "react";
-
-export const Tag = ({ color, name }: Props) => {
-  const [isHovering, setIsHovering] = useState(false);
-
-  const handleMouseEnter = () => setIsHovering(true);
-  const handleMouseLeave = () => setIsHovering(false);
-
-  const handleDeleteClick = () => {
-    // 태그 삭제 로직
-  };
+export const Tag: React.FC<Props> = ({ idx, name, ...props }) => {
+  const colors = [
+    "#6FCF97",
+    "#56CCF2",
+    "#2D9CDB",
+    "#EB5757",
+    "#F2C94C",
+    "#BB6BD9",
+    "#FF9F43",
+    "#4D4D4D",
+    "#BDBDBD",
+    "#6E7B8B",
+  ];
 
   return (
     <div
-      className="tag"
-      style={{ backgroundColor: color }}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      className={style.tag}
+      style={{ backgroundColor: colors[idx] }}
+      {...props}
     >
-      <span>{name}</span>
-      {isHovering && <button onClick={handleDeleteClick}>X</button>}
+      {name}
     </div>
   );
 };
--- frontend/src/components/studentDetail/2.studentDetailCourse/StudentDetailCourse.tsx
+++ frontend/src/components/studentDetail/2.studentDetailCourse/StudentDetailCourse.tsx
@@ -1,12 +1,17 @@
 import { ShortButtonHug } from "../../common/button/Button";
-import { Tag } from "../../common/tag/tag";
+import { Tag } from "../../common/tag/Tag";
+import { useState } from "react";
 
 export const StudentDetailCourse = () => {
+  const [colorIdxList, setColorIdxList] = useState([]);
+
   return (
     <div>
       <h2>수강 과목</h2>
       <ShortButtonHug>영어-내신</ShortButtonHug>
-      <Tag name={"영어-내신"} color={"#6FCF97"}></Tag>
+      {/* <Tag name={"영어-내신"} style={{ backgroundColor: colors[1] }}></Tag> */}
+      {/* <Tag name={"영어-내신"} idx={1}></Tag> */}
+      <Tag name={"영어"} idx={1}></Tag>
       <ShortButtonHug>수학II</ShortButtonHug>
     </div>
   );
