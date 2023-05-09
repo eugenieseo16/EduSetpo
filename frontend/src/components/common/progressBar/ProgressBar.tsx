b29581e932861e3d832e203b1bc2e4550c0c4210
2udy <22udy629@gmail.com>
2023-05-09 16:26:39 +0900

    Feat: progressBar

---
+++ frontend/src/components/common/progressBar/ProgressBar.tsx
@@ -0,0 +1,5 @@
+import style from "./ProgressBar.module.css";
+
+export const ProgressBar = () => {
+  return <div className={style.progressBar}></div>;
+};
---
+++ frontend/src/components/grade/effort/Effort.tsx
@@ -0,0 +1,11 @@
+import { ProgressBar } from "../../common/progressBar/ProgressBar";
+
+export const Effort = () => {
+  return (
+    <div>
+      <h3>성실도</h3>
+      <ProgressBar></ProgressBar>
+      <ProgressBar></ProgressBar>
+    </div>
+  );
+};
