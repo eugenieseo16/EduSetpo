eadb998a2ef0660185bbdaced50ff2eb24086ca3
2udy <22udy629@gmail.com>
2023-05-09 17:41:17 +0900

    Feat: 체크리스트 화면 구현

---
+++ frontend/src/components/common/checkList/CheckList.tsx
@@ -0,0 +1,60 @@
+import style from "./CheckList.module.css";
+import { v4 as uuidv4 } from "uuid";
+import { useCheckListMake } from "./useCheckListMake";
+
+interface BoardProps {
+  headRow: string[];
+  grid: string;
+  data?: any[];
+  url?: string;
+  type: "homework" | "etc";
+}
+
+export const CheckList = ({ headRow, grid, data, type }: BoardProps) => {
+  return (
+    <>
+      {headRow.length !== 0 && (
+        <div
+          className={style.header_container}
+          style={{
+            gridTemplateColumns: grid,
+            animation: "0.7s ease-in-out loadEffect2",
+          }}
+        >
+          {headRow.map((content) => {
+            return (
+              <div
+                key={uuidv4()}
+                style={headRow.length > 4 ? { fontSize: "0.85rem" } : undefined}
+              >
+                {content}
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {data?.map((content, i) => {
+        return (
+          <div
+            key={uuidv4()}
+            className={style.content_container}
+            style={{ gridTemplateColumns: grid }}
+          >
+            {useCheckListMake(type, content).map((contentRow: any) => {
+              return (
+                <div
+                  style={{
+                    animation: `${(i + 1) * 0.3}s ease-in-out loadEffect3`,
+                  }}
+                  key={uuidv4()}
+                >
+                  {contentRow}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </>
+  );
+};
--- frontend/src/pages/sessionDetail/SessionDetail.tsx
+++ frontend/src/pages/sessionDetail/SessionDetail.tsx
@@ -3,12 +3,38 @@ import { ShortButtonHug } from "../../components/common/button/Button";
 import { SessionNote } from "../../components/sessionDetail/sessionNote/SessionNote";
 import { SessionSchedule } from "../../components/sessionDetail/sessionSchedule/SessionSchedule";
 import { SessionHeader } from "../../components/sessionDetail/sessionHeader/SessionHeader";
+import { CheckList } from "../../components/common/checkList/CheckList";
+
+interface Notice {
+  isCompleted: boolean;
+  session: number;
+  content: string;
+}
+
 export const SessionDetail = () => {
   // const navigate = useNavigate();
 
   const onClick = () => {
     history.back();
   };
+
+  const notice: Array<Notice> = [
+    {
+      isCompleted: false,
+      session: 3,
+      content: "첫번째 숙제",
+    },
+    {
+      isCompleted: true,
+      session: 3,
+      content: "두번째 과제",
+    },
+    {
+      isCompleted: false,
+      session: 3,
+      content: "세번째 안해",
+    },
+  ];
 
   return (
     <div>
@@ -18,7 +44,13 @@ export const SessionDetail = () => {
       <SessionHeader />
       <SessionSchedule />
       <SessionNote />
-      <h3>숙제 체크</h3>
+      <CheckList
+        data={notice}
+        grid={"30% 50% 20%"}
+        headRow={["완료여부", "내용", "회차"]}
+        type={"homework"}
+        url={"/mypage/notice/n"}
+      />
       <div>프로그래스바 컴포넌트 들어갈 자리</div>
       <div>체크리스트 컴포넌트 들어갈 자리</div>
     </div>
