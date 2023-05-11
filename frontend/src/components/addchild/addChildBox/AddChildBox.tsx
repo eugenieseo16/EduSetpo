3142eba077bbb2ec2f701c14afca6f77d3dd3c65
yujinseo <00yujin00@naver.com>
2023-05-11 17:07:15 +0900

    Feat: 자식등록 API연결

--- frontend/src/components/addchild/addChildBox/AddChildBox.tsx
+++ frontend/src/components/addchild/addChildBox/AddChildBox.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import styles from './AddChildBox.module.scss';
 import { LongButton } from '../../common/button/Button';
 import axios from 'axios';
-import Modal from '../../common/modal/Modal';
-import AddChildModal from '../addChildModal/AddChildModal';
+import styles from './AddChildBox.module.scss';
 
-export const AddChildBox: React.FC = () => {
-  const [studentLessonId, setStudentLessonId] = useState<string>('');
-  const [childName, setChildName] = useState<string>('');
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const parentId = Math.floor(Math.random() * 1000); // your parent id goes here
+interface AddChildBoxProps {
+  studentLessonId: any;
+  setStudentLessonId: (id: any) => void;
+  setModalOpen: (isOpen: boolean) => void;
+}
 
+export const AddChildBox: React.FC<AddChildBoxProps> = ({
+  studentLessonId,
+  setStudentLessonId,
+  setModalOpen,
+}) => {
   const checkStudentLessonId = async (): Promise<boolean> => {
     try {
       const response = await axios.get(
-        // `https://edusepo.com/api/studentLesson/${studentLessonId}`
         `http://localhost:8080/studentLesson/${studentLessonId}`
       );
       if (response.status === 200) {
@@ -33,26 +34,6 @@ export const AddChildBox: React.FC = () => {
     }
   };
 
-  const addChild = async (): Promise<void> => {
-    try {
-      const response = await axios.post(
-        // 'https://edusepo.com/api/parent/children',
-        'http://localhost:8080/parent/children',
-        {
-          parent_id: parentId,
-          student_lesson_id: studentLessonId,
-          child_name: childName,
-        }
-      );
-      if (response.status === 200) {
-        alert('Child added successfully');
-      }
-    } catch (error) {
-      alert('Error adding child');
-    }
-    setModalOpen(false);
-  };
-
   return (
     <div className={styles['add-child-container']}>
       <h2>인증번호 입력</h2>
@@ -65,18 +46,6 @@ export const AddChildBox: React.FC = () => {
       <LongButton variant="success" onClick={handleModalOpen}>
         등록
       </LongButton>
-      <AddChildModal isOpen={modalOpen} handleClose={() => setModalOpen(false)}>
-        <h1>Child Name</h1>
-        <input
-          type="text"
-          placeholder="Enter Child Name"
-          value={childName}
-          onChange={e => setChildName(e.target.value)}
-        />
-        <LongButton variant="success" onClick={addChild}>
-          등록
-        </LongButton>
-      </AddChildModal>
     </div>
   );
 };
--- frontend/src/components/addchild/addChildModal/AddChildModal.tsx
+++ frontend/src/components/addchild/addChildModal/AddChildModal.tsx
@@ -1,31 +1,69 @@
+// AddChildModal.tsx
 import React from 'react';
-import './AddChildModal.module.scss';
+import axios from 'axios';
+import styles from './AddChildModal.module.scss';
+import { LongButton } from '../../common/button/Button';
 
 interface AddChildModalProps {
   isOpen: boolean;
   handleClose: () => void;
-  children: React.ReactNode;
+  childName: string;
+  setChildName: (name: string) => void;
+  setModalOpen: (isOpen: boolean) => void;
+  studentLessonId: number;
 }
 
-const AddChildModal: React.FC<AddChildModalProps> = ({
+export const AddChildModal: React.FC<AddChildModalProps> = ({
   isOpen,
   handleClose,
-  children,
+  childName,
+  setChildName,
+  setModalOpen,
+  studentLessonId,
 }) => {
+  const parentId = Math.floor(Math.random() * 1000);
+
+  const addChild = async (): Promise<void> => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/parent/children',
+        {
+          childName: childName,
+          parentId: parentId,
+          studentLessonId: studentLessonId,
+        }
+      );
+      if (response.status === 200) {
+        alert('Child added successfully');
+        handleClose();
+      }
+    } catch (error) {
+      alert('Error adding child');
+      console.error(error);
+    }
+  };
+
   return (
     <>
       {isOpen && (
-        <div className="modal">
-          <div className="add-child-container">
-            <button className="close-button" onClick={handleClose}>
+        <div className={styles.modal}>
+          <div className={styles['add-child-container']}>
+            <button className={styles['close-button']} onClick={handleClose}>
               X
             </button>
-            {children}
+            <h3>이름 등록</h3>
+            <input
+              type="text"
+              placeholder="자녀의 이름을 입력해주세요"
+              value={childName}
+              onChange={e => setChildName(e.target.value)}
+            />
+            <LongButton variant="success" onClick={addChild}>
+              등록
+            </LongButton>
           </div>
         </div>
       )}
     </>
   );
 };
-
-export default AddChildModal;
