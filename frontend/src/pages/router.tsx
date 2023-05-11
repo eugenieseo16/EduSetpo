f511b8495a39bae8e91190ec9387e4acfd27a8d9
OYJ <yeonjinnieoh@gmail.com>
2023-05-11 16:26:04 +0900

    Chore : 빈페이지 생성, 파일 구조 수정

--- frontend/src/pages/router.tsx
+++ frontend/src/pages/router.tsx
@@ -1,79 +1,89 @@
-import { RouteObject } from "react-router-dom";
-import { ClassManagement } from "./classManagement/ClassManagement";
-import { SignUp } from "./signUp/SignUp";
-import { NomalPage } from "./NomalPage";
-import { Chart } from "./chart/Chart";
-import { Home } from "./home/Home";
-import { Schedule } from "./schedule/Schedule";
-import { Student } from "./studentDetail/StudentDetail";
-import { MyPage } from "./myPage/MyPage";
-import { ParentsMain } from "./parentsMain/ParentsMain";
-import { SessionDetail } from "./sessionDetail/SessionDetail";
-import { Grade } from "./grade/Grade";
+import { RouteObject } from 'react-router-dom';
+import { ClassManagement, ClassCreate, ClassDetail } from './classManagement';
+import { SignUp } from './signUp/SignUp';
+import { NomalPage } from './NomalPage';
+import { Chart } from './chart/Chart';
+import { Home } from './home/Home';
+import { Schedule } from './schedule/Schedule';
+import { Student } from './studentDetail/StudentDetail';
+import { MyPage } from './myPage/MyPage';
+import { ParentsMain } from './parentsMain/ParentsMain';
+import { SessionDetail } from './sessionDetail/SessionDetail';
+import { Grade } from './grade/Grade';
 
 const router: RouteObject[] = [
   {
-    path: "/",
+    path: '/',
     element: <SignUp />,
     children: [],
   },
   {
-    path: "/tutor",
+    path: '/tutor',
     element: <NomalPage />,
     children: [
       {
-        path: "",
+        path: '',
         element: <Home />,
         children: [],
       },
       {
-        path: "schedule",
+        path: 'schedule',
         element: <Schedule />,
         children: [],
       },
       {
-        path: "class",
+        path: 'class',
         element: <ClassManagement />,
         children: [],
       },
       {
-        path: "student",
+        path: 'class/create',
+        element: <ClassCreate />,
+        children: [],
+      },
+      {
+        path: 'class/id',
+        element: <ClassDetail />,
+        children: [],
+      },
+      {
+        path: 'student',
         element: <Student />,
         children: [],
       },
       {
-        path: "student/session-detail",
+        path: 'student/session-detail',
         element: <SessionDetail />,
         children: [],
       },
       {
-        path: "mypage",
+        path: 'mypage',
         element: <MyPage />,
         children: [],
       },
       {
-        path: "student/grade",
+        path: 'student/grade',
         element: <Grade />,
         children: [],
       },
     ],
   },
   {
-    path: "/parents",
+    path: '/parents',
     element: <NomalPage />,
     children: [
       {
-        path: "",
+        path: '',
         element: <ParentsMain />,
         children: [],
       },
       {
-        path: "chart",
+        path: 'chart',
         element: <Chart />,
         children: [],
       },
       {
-        path: "mypage",
+        path: 'mypage',
         element: <MyPage />,
         children: [],
       },
