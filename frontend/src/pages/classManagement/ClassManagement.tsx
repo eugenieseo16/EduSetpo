dc359cff329f594040ce0fa87464170a1084be71
OYJ <yeonjinnieoh@gmail.com>
2023-05-11 14:41:59 +0900

    Design : Long Button 길이,  button & input 글로벌 스타일링

--- frontend/src/pages/classManagement/ClassManagement.tsx
+++ frontend/src/pages/classManagement/ClassManagement.tsx
@@ -2,11 +2,15 @@ import style from './ClassManagement.module.scss';
 import { Tag } from '../../components/common/tag/Tag';
 import { ClassCard } from '../../components/classManagement/ClassCard';
 
+import { LongButton } from '../../components/common/button/Button';
+
 export const ClassManagement = () => {
   return (
     <div className={style.container}>
       <h1>수업 목록</h1>
       <ClassCard />
+
+      <LongButton variant="success">수업 등록하기</LongButton>
     </div>
   );
 };
