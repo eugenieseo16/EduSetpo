8b42ba23a7446678695998f158aaa9d57b039a69
2udy <22udy629@gmail.com>
2023-05-15 20:51:06 +0900

    Feat: 회차 상세 변경 90% 완료

--- frontend/src/pages/sessionDetail/SessionDetail.tsx
+++ frontend/src/pages/sessionDetail/SessionDetail.tsx
@@ -6,46 +6,68 @@ import { CheckList } from '../../components/common/checkList/CheckList';
 import { ProgressBar } from '../../components/common/progressBar/ProgressBar';
 import { Homework } from '../../types/homework';
 import { GetHomework } from '../../api/homeworkApis';
+import { SessionStudentHomeworkList } from '../../components/sessionDetail/sessionStudentList/SessionStudentHomeworkList';
+import { v4 as uuidv4 } from 'uuid';
+import { Student } from '../student/Student';
 
 export const SessionDetail = () => {
   // const navigate = useNavigate();
 
-  const studentId = 1;
-  // const studentId = parseInt(useLocation().pathname.split('/')[1],10);
-  const sessionId = 1;
-  // const sessionId = parseInt(useLocation().pathname.split('/')[1],10);
-
-  const [homeworks, setHomeworks] = useState<Homework[] | undefined>();
+  // const [session, setSession] = useState()
+  // 내일 세션 api 연결되면 위의 useState로 변경
+  const session = {
+    sessionId: 1,
+    isCompleted: false,
+    lesson: {
+      lessonName: '수학4',
+    },
+    now: 3,
+    total: 12,
+    students: [
+      { studentId: 1, name: '강잼민1' },
+      { studentId: 2, name: '강잼민2' },
+      { studentId: 3, name: '강잼민3' },
+    ],
+    memo: null,
+    actualDate: [2023, 7, 10],
+    startTime: [8, 4, 18],
+    endTime: [8, 4, 18],
+  };
 
   useEffect(() => {
-    const fetchHomeworks = async () => {
+    const fetchSession = async () => {
       try {
-        const fetchedHomeworks = await GetHomework(studentId, sessionId);
-        setHomeworks(fetchedHomeworks);
+        // 세션 정보 API 연결
+        // const fetchSession = await 세션 겟하는 api 함수
+        // setSession(fetchSession)
       } catch (error) {
-        console.error('Error fetching homeworks:', error);
+        console.log(error);
       }
     };
-
-    fetchHomeworks();
-  }, [sessionId, studentId]);
+  }, [session]);
 
   return (
     <div>
       {/* <h1>회차 정보 받아와서 학생 이름 표시</h1> */}
-      <h1>강잼민</h1>
-      <SessionHeader />
-      <SessionSchedule />
+      <SessionHeader
+        isCompleted={session.isCompleted}
+        lessonName={session.lesson.lessonName}
+      />
+      <SessionSchedule
+        actualDate={session.actualDate}
+        startTime={session.startTime}
+        endTime={session.endTime}
+      />
       <SessionNote />
       {/* <ProgressBar value={75} /> */}
-      <CheckList
-        data={homeworks}
-        grid={'30% 50% 20%'}
-        headRow={['완료여부', '내용', '회차']}
-        type={'homework'}
-        url={'/mypage/notice/n'}
-      />
-      <div></div>
+      {session.students.map(student => (
+        <SessionStudentHomeworkList
+          key={uuidv4()}
+          studentName={student.name}
+          studentId={student.studentId}
+          sessionId={session.sessionId}
+        />
+      ))}
     </div>
   );
 };
