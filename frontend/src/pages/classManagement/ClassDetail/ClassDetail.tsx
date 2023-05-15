73237c4e5171371da7aa8384e016cb9be69f511f
yeonjinnieoh <yeonjinnieoh@gmail.com>
2023-05-16 00:33:05 +0900

    Feat : 수업 상세 UI

--- frontend/src/pages/classManagement/ClassDetail/ClassDetail.tsx
+++ frontend/src/pages/classManagement/ClassDetail/ClassDetail.tsx
@@ -1,26 +1,17 @@
 import style from './ClassDetail.module.scss';
 import { Tag } from '../../../components/common/tag/Tag';
-import { ShortButtonHug } from '../../../components/common/button/Button';
+import {
+  LongButton,
+  ShortButtonHug,
+} from '../../../components/common/button/Button';
 import { ClassInfo } from '../../../components/classDetail/ClassInfo';
 
 export const ClassDetail = () => {
   return (
     <div>
       <ClassInfo />
-      <div className={style.tagContainer}>
-        <Tag name="세명중학교" idx={1} />
-        <Tag name="중2" idx={2} />{' '}
-      </div>
 
-      <div className={style.scheduleContainer}>
-        <h3>일정:</h3>
-        <p>월, 목 15:00 ~ 17:00</p>
-        <ShortButtonHug variant="primary">수정</ShortButtonHug>
-      </div>
-
-      <h6>수강 학생:</h6>
-      <h3>강잼민</h3>
-      <h3>이금쪽</h3>
+      <LongButton>수정</LongButton>
     </div>
   );
 };
