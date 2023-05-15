a12a8e29af82932cd757698cd30e7952d1cd450b
2udy <22udy629@gmail.com>
2023-05-15 18:04:40 +0900

    Feat: 부모 리코일

--- frontend/src/atoms/user.atom.ts
+++ frontend/src/atoms/user.atom.ts
@@ -9,13 +9,14 @@ export const tutorInfoState = atom({
   },
 });
 
-// export const parentInfoState = atom({
-//   key: 'parentInfo',
-//   default: {
-//     parentId: 0,
-//     parentName: '',
-//   },
-// });
+export const parentInfoState = atom({
+  key: 'parentInfo',
+  default: {
+    parentId: 0,
+    parentName: '',
+    parentEmail: '',
+  },
+});
 
 // 컴포넌트에서 가져다 쓸 때 아래처럼 쓰면 댐...
 // const [userInfo, setUserInfo] = useRecoilState(userInfoState);
