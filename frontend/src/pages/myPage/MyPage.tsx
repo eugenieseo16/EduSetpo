5839ef3db7022a6e170ebf080467bf1d222f4d77
juggorr <juggorr@gmail.com>
2023-05-18 13:42:08 +0900

    Feat: 마이페이지 로그아웃 추가

--- frontend/src/pages/myPage/MyPage.tsx
+++ frontend/src/pages/myPage/MyPage.tsx
@@ -2,9 +2,21 @@ import { useLocation } from 'react-router-dom';
 import { TutorInfoBar } from '../../components/myPage/TutorInfoBar/TutorInfoBar';
 import { TutorMenus } from '../../components/myPage/TutorMenus/TutorMenus';
 import { ParentEdit } from '../parentEdit/ParentEdit';
+import { LongButton } from '../../components/common/button/Button';
+import { useNavigate } from 'react-router-dom';
+import { tutorInfoState } from '../../atoms/user.atom';
+import { useRecoilState } from 'recoil';
 
 export const MyPage = () => {
   const location = useLocation().pathname.split('/')[1];
+  const navigate = useNavigate();
+  const [tutorInfo, setTutorInfo] = useRecoilState(tutorInfoState);
+  // 로그아웃하고 메인화면으로 보내는 함수
+  const handleLogout = () => {
+    setTutorInfo(null);
+    localStorage.removeItem('access_token');
+    navigate('/');
+  };
 
   return (
     <div>
@@ -12,6 +24,9 @@ export const MyPage = () => {
         <>
           <TutorInfoBar />
           <TutorMenus />
+          <LongButton variant="danger" onClick={handleLogout}>
+            로그아웃
+          </LongButton>
         </>
       ) : (
         <ParentEdit />
